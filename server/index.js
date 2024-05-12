ba26deff702e8da3d3b7fb4920255801c9c247bd
umang00arora <92449585+umang00arora@users.noreply.github.com>
2024-05-12 19:36:56 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,6 +23,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 dotenv.config();
 const app = express();
+app.use(cors({
+origin: ["https://attic.vercel.app"], methods: ["POST", "GET"], credentials: true
+}));
 app.use(express.json());
 app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
